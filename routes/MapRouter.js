52265e4eddaa6a73d187d8e28caa129ebccd9cd0
vince <vincent.calonzo@gmail.com>
2024-04-27 13:26:21 -0400

    .

--- routes/MapRouter.js
+++ routes/MapRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const MapModel = require("../model/Map");
 const router = express.Router();
+const cors = require('cors');
+
+router.use(cors())
 
 // Route to add a new map seed
 router.post("/map/add", async (req, res) => {
